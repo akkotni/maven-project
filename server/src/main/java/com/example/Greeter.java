79ef799118183343b92e6aba47ce5ac7c18e3c2b
akkotni <akkotni@gmail.com>
2018-04-02 15:02:34 -0500

    modifying file by checkstyle warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * some one the name of the person
+   * geeting string
+   */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * this is a random message
+ */
+package com.example;
